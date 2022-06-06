b5e5d4a360523b9ac42417a5eb08e72eddfd68b2
tolinlaws <toby3924830@gmail.com>
2022-06-06 21:55:32 +0800

    add Decide Activity and fix double click

---
+++ app/src/main/java/finalProject/teamSeven/truthordarechooser/Decide.kt
@@ -0,0 +1,14 @@
+package finalProject.teamSeven.truthordarechooser;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class Decide : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
--- app/src/main/java/finalProject/teamSeven/truthordarechooser/MainActivity.kt
+++ app/src/main/java/finalProject/teamSeven/truthordarechooser/MainActivity.kt
@@ -3,6 +3,7 @@ package finalProject.teamSeven.truthordarechooser
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.SystemClock
 import android.view.animation.LinearInterpolator
 import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
@@ -12,31 +13,41 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     lateinit var bottle: ImageView
-
+    val delay: Long = 5000
+    var lastClickTime: Long = 0
+    var angle:Float = 0.0F
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottle=findViewById(R.id.bottle)
         bottle.setImageResource(R.drawable.images)
-        bottle.setOnClickListener{ rotate()}
+        bottle.setOnClickListener{
+            val currentTime = SystemClock.uptimeMillis()
+            if (currentTime - lastClickTime >  delay) {
+                lastClickTime = currentTime;
+                rotate()
+            }
+        }
     }
     private fun rotate() {
-        val R=Random.nextInt(1, 25)
-        val F= Random.nextFloat()*10000
-        val round=bottle.animate().rotationBy(F/R).setDuration(2500).setInterpolator(LinearInterpolator())
-        round.start()
+            val R = Random.nextInt(1, 25)
+            val F = Random.nextFloat() * 10000
+            angle = F/R
+            val round = bottle.animate().rotationBy(F / R).setDuration(2500)
+                .setInterpolator(LinearInterpolator())
+            round.start()
 
-        var text:String
-        object : CountDownTimer(2500, 1000) {
+            var text: String
+            object : CountDownTimer(3000, 1000) {
 
-            override fun onTick(millisUntilFinished: Long) {
-                text="seconds remaining: " + millisUntilFinished / 1000
-            }
+                override fun onTick(millisUntilFinished: Long) {
+                    text = "seconds remaining: " + millisUntilFinished / 1000
+                }
 
-            override fun onFinish() {
-                stop_rotate()
-            }
-        }.start()
+                override fun onFinish() {
+                    stop_rotate()
+                }
+            }.start()
     }
     fun stop_rotate()
     {
@@ -49,12 +60,16 @@ class MainActivity : AppCompatActivity() {
     }
     fun truth(){
         val intent = Intent()
+        /*傳遞angle到intent*/
+        intent.putExtra("angle",angle)
         intent.setClass(this@MainActivity,
             Truth::class.java)
         startActivity(intent)
     }
     fun dare(){
         val intent = Intent()
+        /*傳遞angle到intent*/
+        intent.putExtra("angle",angle)
         intent.setClass(this@MainActivity,
             Dare::class.java)
         startActivity(intent)
